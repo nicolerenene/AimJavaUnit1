c63da4caaa679f47a38513a36c0e1158e88a9d76
nicolerenene <64162382+nicolerenene@users.noreply.github.com>
2020-11-11 17:15:35 -0600

    commit

---
+++ src/Assignment1_1.java
@@ -0,0 +1,13 @@
+public class Assignment1_1 {
+    public static void main(String[] arg){
+        String name = "Nicole Perry";
+        String courseName = "Java Web";
+        String hometown = "Omaha, NE";
+        String favoriteDessert = "Tiramisu";
+
+        System.out.println(name);
+        System.out.println(courseName);
+        System.out.println(hometown);
+        System.out.println(favoriteDessert);
+    }
+}
